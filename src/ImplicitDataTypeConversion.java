66260d2fecece7451ca39313c311c707752bf336
arjuntamrakar1 <atamrakar@avaya.com>
2024-12-26 15:57:37 +0530

    New committed

---
+++ src/ImplicitDataTypeConversion.java
@@ -0,0 +1,12 @@
+public class ImplicitDataTypeConversion {
+
+    public static void main(String[] args) {
+        int a=100;
+        System.out.println("Int represent: : "+a);
+        long b=a ;
+        System.out.println("Long represent: : "+b);
+        float c=b ;
+        System.out.println("float represent: : "+c);
+
+    }
+}
